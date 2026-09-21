f3d02ffd9d1f26523e9002ca6db6291d123f89d6
Hoàng Đức Mạnh <hoangducmanh2004789@gmail.com>
2026-09-21 17:29:45 +0700

    fix

--- src/main/java/org/com/lab/UserApplication.java
+++ src/main/java/org/com/lab/UserApplication.java
@@ -1,19 +1,15 @@
 package org.com.lab;
 
 import org.example.javaframework.annotation.EnableFrameworkSecurity;
-import org.example.javaframework.annotation.EnableFrameworkSession;
-import org.example.javaframework.configuration.SecurityConfig;
-import org.example.javaframework.infra.security.JwtProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableFrameworkSecurity
-public class LabApplication {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LabApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
 }
